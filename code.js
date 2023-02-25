4a5c9231eed1410d103658c0b23313099cc5c22d
Piero <114272608+PieroMS@users.noreply.github.com>
2023-02-25 00:23:04 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,26 @@
+const button=document.querySelector('.button');
+const navbar=document.querySelector('.navbar');
+button.addEventListener('click',()=>{
+    navbar.classList.toggle('activo');
+});
+
+const li1=document.querySelector('.li1');
+li1.addEventListener('click',()=>{
+    navbar.classList.toggle('activo');
+});
+const li2=document.querySelector('.li2');
+li2.addEventListener('click',()=>{
+    navbar.classList.toggle('activo');
+});
+const li3=document.querySelector('.li3');
+li3.addEventListener('click',()=>{
+    navbar.classList.toggle('activo');
+});
+const li4=document.querySelector('.li4');
+li4.addEventListener('click',()=>{
+    navbar.classList.toggle('activo');
+});
+const li5=document.querySelector('.li5');
+li5.addEventListener('click',()=>{
+    navbar.classList.toggle('activo');
+});
